ac41154c70c395f6a28bdc38a41e413372dd67b0
sysuev <168555506+sysuev@users.noreply.github.com>
2024-04-30 18:13:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAVIR WEAR</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<header>
+    <div class="header-content">
+        <h1 class="header-title">RAVIR WEAR</h1>
+        <nav>
+            <ul class="nav-menu">
+                <li><a href="#contact" class="contact-link">Связаться</a></li>
+                <li><a href="#social" class="social-link">Соц. сети</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="contact-info" id="contactInfo">
+        <p>Номер: 89297254053</p>
+        <p>Почта: ravir.intl@mail.ru</p>
+    </div>
+    <div class="social-info" id="socialInfo">
+        <p>Телеграм: <a href="https://t.me/ravirwear">ravirwear</a></p>
+        <p>Инстаграм: <a href="https://www.instagram.com/ravir.intl?igsh=a3QxN3Nvbzg1d2Zh">ravir.intl</a></p>
+        <p>ВКонтакте: <a href="https://vk.com/ravir_co">ravir_co</a></p>
+    </div>
+</header>
+<div class="main-content">
+    <div class="product">
+        <a href="#">
+            <img src="https://sun9-45.userapi.com/impg/Epm6ydsWIBSSKIe-ux5pm_qq8iLW-dzwkXVfbg/7ZOaDz2Llro.jpg?size=646x807&quality=95&sign=270c8244d46efffb27130fd7c35b0c12&c_uniq_tag=A9C7caqLiagwTneLfywcYBjc_x5rF8yH_RbXu7zCK1s&type=album" alt="белая">
+        </a>
+        <p class="product-name"><strong>White</strong></p>
+        <span class="price">Цена: 3500р</span>
+        <span class="slogan">Сделай это великим</span>
+    </div>
+    <div class="product">
+        <a href="#">
+            <img src="https://sun9-29.userapi.com/impg/__b6Zhx3xL_TRmfNxZXUNFO0vREQfYGCpY7QTw/vtUaEjQwXLM.jpg?size=646x807&quality=95&sign=1a8fe4e7147d672955a4d0f27c2e7c14&c_uniq_tag=YHyth3nYvza1EMXSk9VvXFBc1dexkA-rnor-FsNdWYE&type=album" alt="чёрная">
+        </a>
+        <p class="product-name"><strong>Black</strong></p>
+        <span class="price">Цена: 3500р</span>
+        <span class="slogan">Сделай это великим</span>
+    </div>
+</div>
+<footer>
+    <p>© 2024 Ravir. Все права защищены.</p>
+</footer>
+<script src="js/script.js"></script>
+</body>
+</html>
